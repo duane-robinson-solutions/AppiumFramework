83def1f9135e636d29b0a285c9342d8e56015495
Duane Robinson <60953176+duane-robinson-solutions@users.noreply.github.com>
2023-03-03 09:33:08 -0700

    Added app capabilities

--- src/main/java/CreateDriverSession.java
+++ src/main/java/CreateDriverSession.java
@@ -1,14 +1,30 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class CreateDriverSession {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
        DesiredCapabilities caps = new DesiredCapabilities();
        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-       caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+       caps.setCapability(MobileCapabilityType.UDID, "Emulator-5554");
+       String appURL = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+               + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
+
+       URL url = new URL ("http://0.0.0.0:4723/wd/hub");
+
+        AppiumDriver driver = new AndroidDriver(url, caps);
+
+
+
+
 
     }
 }
