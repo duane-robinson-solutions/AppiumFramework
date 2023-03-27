5ae48cbe4d74ad4aa33a7e43fa898aa9d0bff448
Duane Robinson <60953176+duane-robinson-solutions@users.noreply.github.com>
2023-03-27 09:55:07 -0600

    Push Update

---
+++ src/test/java/CreateDriverSessionT.java
@@ -0,0 +1,14 @@
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CreateDriverSessionT {
+    public static void main(String[] args) {
+
+       DesiredCapabilities caps = new DesiredCapabilities();
+       caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+       caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+       caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+       caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+    }
+}
